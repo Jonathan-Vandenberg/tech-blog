28dd3543a8e993e5cb4ae988a84e4d39746c9c57
jon-vdb <jonvdberg8@gmail.com>
2022-12-02 14:59:04 +0700

    styling dark mode

--- pages/code2.tsx
+++ pages/code2.tsx
@@ -8,17 +8,16 @@ import "prismjs/components/prism-javascript";
 import "prismjs/themes/prism.css";
 
 export default function App() {
-  let [code, setCode] = useState(`function add(a,b){
-return a+b;}
-
-console.log(add(2, input()))`);
-  let [code64, setCode64] = useState("");
+  // let [decoded, setDecode64] = useState("");
+  // let [dummy, setDummy] = useState("");
+  let [finalResult, setFinalResult] = useState({});
   let [encoded, setEncoded] = useState(false);
-  let [decoded, setDecode64] = useState("");
+  let [code, setCode] = useState(`print("Hello, " + input() + "!")`);
+  let [code64, setCode64] = useState("");
   let [token, setToken] = useState("");
   let [result, setResult] = useState("");
-  let [dummy, setDummy] = useState("");
-  let [finalResult, setFinalResult] = useState({});
+  let [task, setTask] = useState("");
+  let [tokenCode64, setTokenCode64] = useState("");
 
   // useEffect(() => {
   //   getCode();
@@ -26,9 +25,19 @@ console.log(add(2, input()))`);
   // }, [code64]);
 
   function encode() {
-    let encoded = Buffer.from(code, "utf8").toString("base64");
-    console.log(encoded);
-    setCode64(encoded);
+    const encodeSourceCode = async () => {
+      let encoded = Buffer.from(code, "utf8").toString("base64");
+      return setCode64(encoded);
+    };
+
+    const encodeTask = async () => {
+      let encodedToken = Buffer.from(token, "utf8").toString("base64");
+      return setTokenCode64(encodedToken);
+    };
+
+    encodeSourceCode();
+    encodeTask();
+
     setTimeout(() => getToken(), 2000);
     return setEncoded(true);
   }
@@ -42,7 +51,7 @@ console.log(add(2, input()))`);
         "X-RapidAPI-Key": "238f019ceamsh555090a8d52cec6p1178e5jsn2e7cd458b6c3",
         "X-RapidAPI-Host": "codeexec.p.rapidapi.com",
       },
-      data: `{"source_code":"ZnVuY3Rpb24gYWRkKGEsYil7CnJldHVybiBhK2I7fQpjb25zb2xlLmxvZyhhZGQoMiwgaW5wdXQoKSkp","language_id":7,"tasks":[{"stdin":"MTAw"}]}`,
+      data: `{"source_code":"${code64}","language_id":3,"tasks":[{"stdin":"${tokenCode64}"}]}`,
     };
 
     axios
@@ -103,21 +112,26 @@ console.log(add(2, input()))`);
         console.error(error);
       });
   }
-  // console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-  // console.log(
-  //   `Test stdin: ${Buffer.from(
-  //     "ZnVuY3Rpb24gYWRkKGEsYil7CnJldHVybiBhK2I7fQoKY29uc29sZS5sb2coYWRkKDIsIGlucHV0KCkpKQ==",
-  //     "base64"
-  //   ).toString("utf8")}`
-  // );
-  // console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+  console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+  console.log(
+    `source-code: ${Buffer.from(
+      "cHJpbnQoIkhlbGxvLCAiICsgaW5wdXQoKSArICIhIikK",
+      "base64"
+    ).toString("utf8")}`
+  );
+  console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+  console.log(`task: ${Buffer.from("V29ybGQK", "base64").toString("utf8")}`);
+  console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+  console.log(`task: ${task}`);
+  console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+  // V29ybGQK
 
   return (
     <div className="pt-20 container mx-auto min-h-96 w-auto bg-gray-700">
       <Editor
         value={code}
         onValueChange={(code) => setCode(code)}
-        highlight={(code) => highlight(code, languages.js, "js")}
+        highlight={(code) => highlight(code, languages.javascript, "js")}
         padding={10}
         style={{
           fontFamily: '"Fira code", "Fira Mono", monospace',
@@ -125,6 +139,7 @@ console.log(add(2, input()))`);
         }}
       />
       {/* <div>{result}</div> */}
+      <input value={task} onChange={(e) => setTask(e.target.value)} />
       <div className="flex flex-col space-y-3 items-center justify-center">
         <button onClick={() => encode()}>Encode</button>
         <button onClick={() => getCode()}>getCODE</button>
